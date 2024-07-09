0245220ac1f6c9ca0ab8e491cf816418a75adc46
Brian <briancmuhammad@yahoo.com>
2024-07-09 12:42:04 -0400

    initialized js challenge hard project. rating task

---
+++ app.js
@@ -0,0 +1,22 @@
+/* function showRating(rating) {
+  let rate = "";
+
+  for (let i = 0; i < Math.floor(rating); ++i) {
+    rate = rate + "*";
+    if (i !== Math.floor(rating) - 1) {
+      rate = rate + " ";
+    }
+   
+  }
+
+  if(!Number.isInteger(rating)){
+    rate = rate + " .";
+}
+
+
+  return rate;
+}
+
+console.log(showRating(3));
+console.log(showRating(4.5));
+console.log(showRating(0.5));*/
